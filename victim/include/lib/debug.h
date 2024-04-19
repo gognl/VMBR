21f6bf2a87bc3c743c58966217b5b7cf6a8dc7b9
gognl <gonigolan@gmail.com>
2024-04-19 21:51:34 +0300

    Added a bash script for running the project

--- victim/include/lib/debug.h
+++ victim/include/lib/debug.h
@@ -11,7 +11,7 @@
 #define LOG_LEVEL_ERROR 2
 #define CURRENT_LOG_LEVEL LOG_LEVEL_DEBUG
 #define DEBUG_VMEXITS FALSE
-#define DEBUG_ALLOCATIONS FALSE
+#define DEBUG_ALLOCATIONS TRUE
 
 extern void LOG_DEBUG(char_t *s, ...);
 extern void LOG_INFO(char_t *s, ...);
